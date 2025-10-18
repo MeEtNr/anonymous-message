81f9dcf56f4108ec2c9a79d29298916afe898d18
meet <meetrangholiya11@gmail.com>
2025-10-18 10:55:57 +0530

    chaged the model of gemeni ai

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -9,7 +9,7 @@ export async function POST() {
   try {
     const prompt =
       "Create a list of three open-ended and creative questions formatted as a single string, separated by '||'. These questions are for an anonymous social platform and should encourage friendly, diverse conversations. Avoid repeating examples given earlier, and vary the topics widely each time. Focus on fun, positive, or interesting themes without becoming too personal. Make each set of questions feel unique and imaginative. and keep the questions small as well.";
-    const model = google("gemini-2.5-flash");
+    const model = google("gemini-2.0-flash");
     const response = await generateText({
       model: model as unknown as LanguageModel,
       maxOutputTokens: 500,
